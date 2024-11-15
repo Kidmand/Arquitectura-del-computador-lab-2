658982e207ae444b3f76deca2c1fe87806aee7d0
kidmand <daian.gimenez@mi.unc.edu.ar>
2024-11-15 16:38:22 -0300

    feat: se corrigió el código de loop and rooling.

--- quemu-test/daxpy_mejorado.s
+++ quemu-test/daxpy_mejorado.s
@@ -42,27 +42,54 @@
     ldr x9, [x10]
     scvtf d0, x9    
 
-    mov x5, 0 // i = 0
-loop:
+    mov x5, 0   // i = 0
+    mov x6, 0   // x6 = 0
+/*loop:
     cmp x5, x0 
     b.ge end   // if i >= N, end
 
-    ldr d1, [x2, #0] // d1 = X[i]
-    ldr d2, [x3, #0] // d2 = Y[i]
-    fmul d3, d0, d1 // d3 = alpha * X[i]
-    fadd d3, d3, d2 // d3 = alpha * X[i] + Y[i]
-    str d3, [x4, #0] // Z[i] = alpha * X[i] + Y[i]
+    ldr d1, [x2, #0]    // d1 = X[i]
+    ldr d2, [x3, #0]    // d2 = Y[i]
+    fmul d3, d0, d1     // d3 = alpha * X[i]
+    fadd d3, d3, d2     // d3 = alpha * X[i] + Y[i]
+    str d3, [x4, #0]    // Z[i] = alpha * X[i] + Y[i]
 
-    ldr d4, [x2, #8] // d4 = X[i]
-    ldr d5, [x3, #8] // d5 = Y[i]
-    fmul d6, d0, d4 // d6 = alpha * X[i]
-    fadd d6, d6, d5 // d6 = alpha * X[i] + Y[i]
-    str d6, [x4, #8] // Z[i] = alpha * X[i] + Y[i]
+    ldr d4, [x2, #8]    // d4 = X[i]
+    ldr d5, [x3, #8]    // d5 = Y[i]
+    fmul d6, d0, d4     // d6 = alpha * X[i]
+    fadd d6, d6, d5     // d6 = alpha * X[i] + Y[i]
+    str d6, [x4, #8]    // Z[i] = alpha * X[i] + Y[i]
 
-    add x2, x2, 16 // X[] += 16
-    add x3, x3, 16 // Y[] += 16
-    add x4, x4, 16 // X[] += 16
-    add x5, x5, 2 // i++
+    add x2, x2, 16      // X[] += 16
+    add x3, x3, 16      // Y[] += 16
+    add x4, x4, 16      // Z[] += 16
+    add x5, x5, 2       // i++
+    b loop
+end:*/
+
+
+loop:
+    cmp x5, x0           // Comparar i con N
+    b.ge end             // If i >= N, end
+
+    ldr d1, [x2, x6]     // d1 = X[i]
+    ldr d2, [x3, x6]     // d2 = Y[i]
+
+    fmul d3, d0, d1      // d3 = alpha * X[i]
+    fadd d3, d3, d2      // d3 = alpha * X[i] + Y[i]
+    str d3, [x4, x6]     // Z[i] = alpha * X[i] + Y[i]
+
+    add x6, x6, 8        // x6 += 8
+
+    ldr d4, [x2, x6]     // d1 = X[i+1]
+    ldr d5, [x3, x6]     // d2 = Y[i+1]
+
+    fmul d6, d0, d4      // d3 = alpha * X[i+1]
+    fadd d6, d6, d5      // d3 = alpha * X[i+1] + Y[i+1]
+    str d6, [x4, x6]     // Z[i+1] = alpha * X[i+1] + Y[i+1]
+
+    add x5, x5, 2        // i++
+    add x6, x6, 8        // x6 += 8
     b loop
 end:
 
