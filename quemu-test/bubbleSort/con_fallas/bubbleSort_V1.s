345b7d70134b4400de1f03efd38aa8346a5f3d7c
García Daián <132107059+Kidmand@users.noreply.github.com>
2024-11-18 22:00:29 -0300

    Update bubbleSort_V1.s
    se corrigio un pequeño error

--- quemu-test/bubbleSort/con_fallas/bubbleSort_V1.s
+++ quemu-test/bubbleSort/con_fallas/bubbleSort_V1.s
@@ -83,9 +83,8 @@ loop_i:
                 CMP X5, x7
                 B.LE fail_if                    // Si arr[j] <= arr[j+1], saltar intercambio
 
-                MOV X8, X5
                 STR X7, [X10, X2, LSL #3]       // arr[j] = arr[j+1]
-                STR X8, [X10, X6, LSL #3]       // arr[j+1] = arr[j]
+                STR X5, [X10, X6, LSL #3]       // arr[j+1] = arr[j]
 
             fail_if:
                 ADD X2, X2, #1                  // j++
